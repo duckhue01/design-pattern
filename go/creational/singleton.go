e9a34a58a5409ad13e17394203a40056a74719f5
duckhue01 <duckhuejs@gmail.com>
2021-12-27 23:16:41 +0700

    m

---
+++ go/creational/singleton.go
@@ -0,0 +1,6 @@
+package creational
+
+
+
+// singleton is creational design pattern that lets you ensure that a class has only one instance,
+// while providing a global access point to this instance
